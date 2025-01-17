20e8db3ee5c11470e3a455257d50b9a359ffa190
aldairgc <dev@aldairgc.com>
2025-01-17 05:53:31 -0300

    Domain implementation

---
+++ src/main/java/com/aldairgc/bank_dev/domain/model/Feature.java
@@ -0,0 +1,8 @@
+package com.aldairgc.bank_dev.domain.model;
+
+import jakarta.persistence.Entity;
+
+@Entity(name = "tb_feature")
+public class Feature extends BaseItem {
+
+}
---
+++ src/main/java/com/aldairgc/bank_dev/domain/repository/UserRepository.java
@@ -0,0 +1,7 @@
+package com.aldairgc.bank_dev.domain.repository;
+
+import com.aldairgc.bank_dev.domain.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+}
